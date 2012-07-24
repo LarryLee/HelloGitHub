d5fd1a62aa73feb00877641d16d5ba63aa404a9c
Uniy Xu <xu_yunan@163.com>
2012-07-24 22:08:37 +0800

    some files

---
+++ HelloGitHub/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloGitHub
+//
+//  Created by Uniy on 12-7-24.
+//  Copyright (c) 2012年 Uniy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloGitHub/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloGitHub
+//
+//  Created by Uniy on 12-7-24.
+//  Copyright (c) 2012年 Uniy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
